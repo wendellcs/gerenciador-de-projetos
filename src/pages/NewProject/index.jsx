8420bc8f894994d2c0270d2999be0708320da99e
Wendell <wendellcs131@gmail.com>
2025-01-09 01:12:13 -0300

    Finished page style

--- src/pages/NewProject/index.jsx
+++ src/pages/NewProject/index.jsx
@@ -1,18 +1,24 @@
 import './newProject.sass'
 import Header from '../../components/Header'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { IoCheckmarkDoneCircleOutline, IoCodeWorkingSharp } from "react-icons/io5";
+import { CiImageOn } from "react-icons/ci";
 import { MdCancel } from "react-icons/md";
 import { GiNightSleep } from "react-icons/gi";
 
 export default function NewProject(){
-    const [projectName, setProjectName] = useState('')
-
     const [checkNotStarted, setCheckNotStarted] = useState(true)
     const [checkPaused, setCheckPaused] = useState(false)
     const [checkInProgress, setCheckInProgress] = useState(false)
     const [checkCompleted, setCheckCompleted] = useState(false)
     const [checkMessage, setCheckMessage] = useState(['not-started', 'Projeto ainda não iniciado'])
+    
+    const [projectName, setProjectName] = useState('')
+    const [projectStatus, setProjectStatus] = useState('')
+    const [projectDescription, setProjectDescription] = useState('')
+    const [startDate, setStartDate] = useState('')
+    const [completionDate, setCompletionDate] = useState('')
+    const [projectImage, setProjectImage] = useState(null)
 
     function check(element){
         switch(element){
@@ -50,7 +56,24 @@ export default function NewProject(){
                 break
         }
 
-        
+        setProjectStatus(checkMessage[0])
+    }
+
+    function addProject(e){
+        e.preventDefault()
+
+        const dates = [startDate, completionDate]
+
+        const data = {
+            name: projectName,
+            status: projectStatus,
+            date: dates,
+            description: projectDescription,
+            image: projectImage
+        }
+
+        // Guardar os dados no storage
+        console.log(data)
     }
 
     return (
@@ -92,7 +115,7 @@ export default function NewProject(){
 
                     <div className="form-box">
                         <label htmlFor = 'project-description'>Descrição do projeto</label>
-                        <textarea placeholder='Descreva seu projeto' className='description' id="project-description"></textarea>
+                        <textarea placeholder='Descreva seu projeto' className='description' id="project-description" value={projectDescription} onChange={(e) => setProjectDescription(e.target.value)}></textarea>
                     </div>
 
                     <div className="form-box">
@@ -112,6 +135,18 @@ export default function NewProject(){
                             </div>
                         </div>
                     </div>
+
+                    <div className='form-box'>
+                        <label htmlFor = 'project-image'>Print do projeto</label>
+
+                        <div className="preview">
+                            <CiImageOn className='icon primary'/>
+                        </div>
+
+                        <input type="file" className='project-image' onChange={(e) => setProjectImage(e.target.files[0])}/>
+                    </div>
+
+                    <button className='btn add-project form' onClick={(e) => addProject(e)}>Adicionar</button>
                 </form>
             </div>
         </main>
